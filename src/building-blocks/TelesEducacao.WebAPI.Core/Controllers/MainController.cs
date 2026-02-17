843c2813d8fb5e1e4c71dfb59394236ec87bbc19
Rafael Secco <raf.secco@gmail.com>
2026-02-17 09:47:13 -0300

    Add WebAPI Core project and implement ConteudoController

---
+++ src/building-blocks/TelesEducacao.WebAPI.Core/Controllers/MainController.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using TelesEducacao.Core.Communication.Mediator;
+using TelesEducacao.Core.Messages.CommomMessages.Notifications;
+
+namespace TelesEducacao.WebAPI.Core.Controllers;
+
+public class MainController : Controller
+{
+	private readonly IMediatorHandler _mediatorHandler;
+	private readonly DomainNotificationHandler _notifications;
+
+	public MainController(IMediatorHandler mediatorHandler,
+						  INotificationHandler<DomainNotification> notifications)
+	{
+		_mediatorHandler = mediatorHandler;
+		_notifications = (DomainNotificationHandler)notifications;
+	}
+
+	protected bool OperacaoValida()
+	{
+		return !_notifications.TemNotificacoes();
+	}
+
+	protected void NotificarErro(string codigo, string mensagem)
+	{
+		_mediatorHandler.PublicarNotificacao(new DomainNotification(codigo, mensagem));
+	}
+
+	protected string ObterMensagemErro()
+	{
+		var notificacoes = _notifications.ObterNotificacoes();
+		var mensagem = string.Join(Environment.NewLine, notificacoes.Select(n => n.Value));
+		return mensagem;
+	}
+}
