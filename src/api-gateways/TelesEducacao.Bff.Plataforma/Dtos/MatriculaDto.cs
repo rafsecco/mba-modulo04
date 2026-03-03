1ad263f1ae8fa48ede1f26d560d47fafecf95b5e
Karollainny Teles <karollainny@aqua.com.br>
2026-03-02 21:27:19 -0300

    add endpoints ConcluirAula, ObterMatriculasPorAlunoId e ObterTodos

---
+++ src/api-gateways/TelesEducacao.Bff.Plataforma/Dtos/MatriculaDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TelesEducacao.Bff.Plataforma.Dtos;
+
+public class MatriculaDto
+{
+    public Guid Id { get; init; }
+    public Guid CursoId { get; init; }
+
+    public MatriculaStatus MatriculaStatus { get; init; }
+}
+
+public enum MatriculaStatus
+{
+    Ativa,
+    PendentePagamento,
+    Cancelada,
+    Concluida,
+}
