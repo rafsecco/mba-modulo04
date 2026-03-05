0d6ebd9e685e932ecc2a9b75d34cfc766652aa8a
Karollainny Teles <karollainny@aqua.com.br>
2026-03-04 21:38:08 -0300

    adiciona endpoints AdicionarMatricula e SolicitarFinalizacaoCurso

--- src/api-gateways/TelesEducacao.Bff.Plataforma/Services/AlunoService.cs
+++ src/api-gateways/TelesEducacao.Bff.Plataforma/Services/AlunoService.cs
@@ -13,6 +13,10 @@ public interface IAlunoService
     Task<IEnumerable<MatriculaDto>> ObterMatriculasPorAlunoId(Guid id, CancellationToken cancellationToken);
 
     Task<bool> ConcluirAula(Guid id, Guid aulaId, CancellationToken cancellationToken);
+
+    Task<bool> SolicitarFinalizacaoCurso(Guid matriculaId, CancellationToken cancellationToken);
+
+    Task<bool> AdicionarMatricula(Guid id, Guid cursoId, AdicionarMatriculaDto matriculaDto, CancellationToken cancellationToken);
 }
 
 public class AlunoService : Service, IAlunoService
@@ -62,6 +66,18 @@ public class AlunoService : Service, IAlunoService
     public async Task<bool> ConcluirAula(Guid id, Guid aulaId, CancellationToken cancellationToken)
     {
         var response = await _httpClient.PostAsync($"/alunos/{id}/Matricula/{aulaId}/AulasConcluidas", null, cancellationToken);
+        return TratarErrosResponse(response);
+    }
+
+    public async Task<bool> SolicitarFinalizacaoCurso(Guid matriculaId, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.PostAsync($"/alunos/{matriculaId}/matricula/certificados", null, cancellationToken);
+        return TratarErrosResponse(response);
+    }
+
+    public async Task<bool> AdicionarMatricula(Guid id, Guid cursoId, AdicionarMatriculaDto matriculaDto, CancellationToken cancellationToken)
+    {
+        var response = await _httpClient.PostAsJsonAsync($"/alunos/{id}/Matricula/{cursoId}", matriculaDto, cancellationToken);
         return TratarErrosResponse(response);
     }
 }
