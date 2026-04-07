06960b4a1710b9cf332e927526a00de91c6872a3
Karollainny Teles <karollainny@aqua.com.br>
2026-04-07 20:36:28 -0300

    remove webapp api

--- src/services/TelesEducacao.WebApp.API/AccessControl/IUserService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TelesEducacao.WebApp.API.AccessControl;
-
-public interface IUserService
-{
-    Task<Guid?> RegisterAsync(string email, string senha, string roleName, CancellationToken cancellationToken);
-
-    Task<Guid?> LoginAsync(string email, string senha, CancellationToken cancellationToken);
-}
