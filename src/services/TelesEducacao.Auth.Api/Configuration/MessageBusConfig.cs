726ed5de075ab650ae21b488c60ef51d80b0b3d5
Guilherme <guilherme.santanna.it@gmail.com>
2026-04-23 23:33:18 -0300

    Acerto nome pasta

---
+++ src/services/TelesEducacao.Auth.Api/Configuration/MessageBusConfig.cs
@@ -0,0 +1,13 @@
+﻿using TelesEducacao.Core.Utils;
+using TelesEducacao.MessageBus;
+
+namespace TelesEducacao.Auth.API.Configuration;
+
+public static class MessageBusConfig
+{
+    public static void AddMessageBusConfiguration(this IServiceCollection services,
+    IConfiguration configuration)
+    {
+        services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
+    }
+}
